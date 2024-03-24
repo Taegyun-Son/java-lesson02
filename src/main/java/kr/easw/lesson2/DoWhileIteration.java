8792385104146c02a4010e50c544675d157a610c
Taegyun Son <oks502503@naver.com>
2024-03-24 09:57:01 +0900

    솔루션 업로드

--- src/main/java/kr/easw/lesson2/DoWhileIteration.java
+++ src/main/java/kr/easw/lesson2/DoWhileIteration.java
@@ -3,28 +3,23 @@ package kr.easw.lesson2;
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * 해당 클래스는 간단한 블랙잭 게임의 예제입니다.
- *
- * do-while로 적절한 코드를 작성하였다면, 간단한 블랙잭 게임이 진행됩니다.
- */
 public class DoWhileIteration {
     private static final Random random = new Random();
-
     private static final Scanner scanner = new Scanner(System.in);
-
     private static final int TARGET_VALUE = 21;
-
     private static int currentValue = 0;
 
+
     public static void main(String[] args) {
+        // TODO Auto-generated method stub
         System.out.println("시작합니다.");
         checkValue();
+
         if (currentValue >= TARGET_VALUE) {
             System.out.println("최종 점수: 0");
             System.out.println("패배했습니다.");
         } else {
-            System.out.println("최종 점수: " + (TARGET_VALUE - (TARGET_VALUE - currentValue)));
+            System.out.println("최종 점수: " + (TARGET_VALUE - (TARGET_VALUE-currentValue)));
         }
     }
 
@@ -37,16 +32,21 @@ public class DoWhileIteration {
      * - do-while문을 반드시 이용해야 합니다.
      */
     private static void checkValue() {
-        throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+        do {
+            increaseValue();
+        }while(!stopGame());
     }
 
     public static boolean stopGame() {
+
         if (TARGET_VALUE <= currentValue)
             return true;
         String nextValue = null;
+
         while (true) {
             System.out.print("계속 합니까? (Y / N) : ");
             String input = scanner.next();
+
             if (input.equals("Y") || input.equals("N")) {
                 nextValue = input;
                 break;
@@ -55,7 +55,6 @@ public class DoWhileIteration {
         }
         return nextValue.equals("N");
     }
-
 
     private static void increaseValue() {
         currentValue += random.nextInt(10) + 1;
--- src/main/java/kr/easw/lesson2/ForWhileIteration.java
+++ src/main/java/kr/easw/lesson2/ForWhileIteration.java
@@ -6,34 +6,74 @@ public class ForWhileIteration {
     private static int currentIteration = 0;
 
     public static void main(String[] args) {
+
+        // TODO Auto-generated method stub
+
         iterateTenTime();
+
         iterateTwentyTime();
+
         if (currentIteration != TARGET_ITERATION) {
+
             System.out.println("오답입니다.");
+
         } else {
+
             System.out.println("정답입니다.");
+
         }
+
     }
 
     /**
+
      *  해당 메서드는 다음과 같은 역할을 가져야 합니다 :
+
      *  for문을 이용해, 정확히 10번 {@code runTask} 메서드를 실행해야 합니다.
+
      */
+
     private static void iterateTenTime() {
-        throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+
+        for (int i = 0; i < 10; i++) {
+
+            runTask();
+
+        }
+
     }
 
+
+
     /**
+
      *  해당 메서드는 다음과 같은 역할을 가져야 합니다 :
+
      *  while문을 이용해, 정확히 20번 {@code runTask} 메서드를 실행해야 합니다.
+
      */
+
     private static void iterateTwentyTime() {
+
         int iterateCounter = 0;
-        throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+
+        while(iterateCounter<20) {
+
+            runTask();
+
+            iterateCounter++;
+
+        }
+
     }
+
+
 
     private static void runTask() {
+
         currentIteration++;
+
         System.out.println(currentIteration + "번 반복되었습니다.");
+
     }
 }
